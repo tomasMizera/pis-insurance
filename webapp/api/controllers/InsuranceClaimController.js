87d852de594af6031fbe59c98e77e5174a4174f0
tomasMizera <tomas.mizera2@gmail.com>
2019-04-06 14:54:26 +0200

    routing in concrete insurance claim

--- webapp/api/controllers/InsuranceClaimController.js
+++ webapp/api/controllers/InsuranceClaimController.js
@@ -5,6 +5,9 @@ function newInsuranceClaim(req, res) {
     res.view('pages/homepage');
 }
 
+async function getInsuranceClaimDetails(req, res) {
+    throw new Error('It failed!');
+}
 
 module.exports = {
 
@@ -17,6 +20,16 @@ module.exports = {
 
     },
 
+    getInsuranceClaim: (req, res) => {
+        getInsuranceClaimDetails(req, res)
+        .then((data) => {
+            res.view('pages/insuranceClaimForEmployee',data);
+        })
+        .catch((err) => {
+            res.serverError('Ooops.. something wrong happened on the server: ' + err);
+        });
+    },
+
     setInsuranceClaimStatus: (req, res) => {
 
     },
@@ -24,4 +37,24 @@ module.exports = {
     updateInsuranceClaim: (req, res) => {
         
     }
+
+
+    // Just wanted to add dump data to db 
+    // await Vet.create({
+    //     emailAddress: 'vet@ma.om',
+    //     password: 'patrik',
+    //     first_name: 'Patrik',
+    //     last_name: 'Skulavy',
+    //     address: 'J. Holleho 13, Tvrdosovce u Dominika doma',
+    //     phone_number: '0944522011',
+    // });
+    // await Vet.create({
+    //     emailAddress: 'v@ma.com',
+    //     password: 'vet',
+    //     first_name: 'Bethoven',
+    //     last_name: 'Ruzovy',
+    //     address: 'J. Holleho 13, Tvrdosovce u Dominika doma',
+    //     phone_number: '0944522011',
+    //     bank_account: 'SK110092200025125663642'
+    // });
 }
