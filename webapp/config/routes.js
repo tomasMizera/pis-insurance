95f31252af424c68f80b95343f3db391d8ffd285
mmio <dominikvasko96@gmail.com>
2019-05-15 22:56:41 +0200

    Adding smaller changes

--- webapp/config/routes.js
+++ webapp/config/routes.js
@@ -7,12 +7,10 @@
  * For more information on configuring custom routes, check out:
  * https://sailsjs.com/anatomy/config/routes-js
  */
-var express = require('express');
-var serveStatic = require('serve-static');
-var os = require('os');
+const serveStatic = require('serve-static');
+const os = require('os');
 
-const dir = `${os.homedir()}/sailsInsuranceDocs`
-
+const dir = `${os.homedir()}/sailsInsuranceDocs`;
 
 module.exports.routes = {
 
@@ -61,28 +59,28 @@ module.exports.routes = {
   //  ╩ ╩╩  ╩  ╚═╝╝╚╝═╩╝╩  ╚═╝╩╝╚╝ ╩ ╚═╝
   // Note that, in this app, these API endpoints may be accessed using the `Cloud.*()` methods
   // from the Parasails library, or by using those method names as the `action` in <ajax-form>.
-  '/api/v1/account/logout':                           { action: 'account/logout' },
-  'PUT   /api/v1/account/update-password':            { action: 'account/update-password' },
-  'PUT   /api/v1/account/update-profile':             { action: 'account/update-profile' },
-  'PUT   /api/v1/account/update-billing-card':        { action: 'account/update-billing-card' },
-  'PUT   /api/v1/entrance/login':                        { action: 'entrance/login' },
-  'POST  /api/v1/entrance/signup':                       { action: 'entrance/signup' },
-  'POST  /api/v1/entrance/send-password-recovery-email': { action: 'entrance/send-password-recovery-email' },
-  'POST  /api/v1/entrance/update-password-and-login':    { action: 'entrance/update-password-and-login' },
-  'POST  /api/v1/deliver-contact-form-message':          { action: 'deliver-contact-form-message' },
+  '/api/v1/account/logout':                               { action: 'account/logout' },
+  'PUT   /api/v1/account/update-password':                { action: 'account/update-password' },
+  'PUT   /api/v1/account/update-profile':                 { action: 'account/update-profile' },
+  'PUT   /api/v1/account/update-billing-card':            { action: 'account/update-billing-card' },
+  'PUT   /api/v1/entrance/login':                         { action: 'entrance/login' },
+  'POST  /api/v1/entrance/signup':                        { action: 'entrance/signup' },
+  'POST  /api/v1/entrance/send-password-recovery-email':  { action: 'entrance/send-password-recovery-email' },
+  'POST  /api/v1/entrance/update-password-and-login':     { action: 'entrance/update-password-and-login' },
+  'POST  /api/v1/deliver-contact-form-message':           { action: 'deliver-contact-form-message' },
 
 
-  'GET /insuranceClaims':                               { controller: 'InsuranceClaimController', action: 'getInsuranceClaims'},
-  'GET /insuranceClaimStatus':                          { controller: 'InsuranceClaimController', action: 'setInsuranceClaimStatus'},
-  'GET /insuranceClaim/:claimId':                       { controller: 'InsuranceClaimController', action: 'getInsuranceClaim'},
+  'GET /insuranceClaims':                                 { controller: 'InsuranceClaimController', action: 'getInsuranceClaims'},
+  'GET /insuranceClaimStatus':                            { controller: 'InsuranceClaimController', action: 'setInsuranceClaimStatus'},
+  'GET /insuranceClaim/:claimId':                         { controller: 'InsuranceClaimController', action: 'getInsuranceClaim'},
 
 
-  'POST /insuranceClaim':                               { controller: 'InsuranceClaimController', action: 'addInsuranceClaim'},
-  'GET /submitted':                                     { action: 'dashboard/view-submited' },
+  'POST /insuranceClaim':                                 { controller: 'InsuranceClaimController', action: 'addInsuranceClaim'},
+  'GET /submitted':                                       { action: 'dashboard/view-submited' },
 
-  '/documents/*':                                       serveStatic(dir, {skipAssets: true}),
+  '/documents/*':                                         serveStatic(dir, {skipAssets: true}),
 
-  'POST /updateClaim':                                  { controller: 'InsuranceClaimController', action: 'updateInsuranceClaim'},
-  'GET /finalizeClaim/:claimId':                        { controller: 'InsuranceClaimController', action: 'finalizeInsuranceClaim'}
+  'POST /updateClaim':                                    { controller: 'InsuranceClaimController', action: 'updateInsuranceClaim'},
+  'GET /finalizeClaim/:claimId':                          { controller: 'InsuranceClaimController', action: 'finalizeInsuranceClaim'}
   
 };
